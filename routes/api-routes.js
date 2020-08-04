487a3b3390374d4e1ce5423dfb622d3e47f1de16
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-08-04 18:33:41 -0500

    functioning api-routes, CRU works

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
+// create workout
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout => {
@@ -11,7 +12,21 @@ router.post("/api/workouts", ({ body }, res) => {
     })
 });
 
-// for getLastWorkout() in the api.js
+// update workout
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    { _id: params.id }, 
+    { $push: { exercises: body }}
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch (err => {
+      res.status(400).json(err);
+    })
+});
+
+// retrieve workout
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .then(dbWorkout => {
@@ -22,18 +37,11 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.get("/exercise/:id", (req, res) => {
-  Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
+// retrieve workouts in range
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({})
+    .sort({ day: -1 })
+    .limit(7)
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
