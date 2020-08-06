adf79d9588171aab024f2c89566a6766841c6f96
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-08-06 15:06:50 -0500

    comments on html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+// sends html file based on clicked link
 router.get("/exercise", (req, res) => {
   res.sendFile("exercise.html", { root: "public" });
 });
